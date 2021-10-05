a4d7c6e7e064484cbc6423784942778c9a3b64fa
nicolasberti <71272720+nicolasberti@users.noreply.github.com>
2021-10-05 14:00:46 -0300

    fin v0.1

--- converter/bin/Release/tester.bat
+++ converter/bin/Release/tester.bat
@@ -1,8 +1,23 @@
-converter -n FFFFFFFFFF,FFFFF -s 16 -d 10 -v
+convert -n FFFFFFFFFF,FFFFF -s 16 -d 2 -v
 pause
+cls
 
-converter -n FFFFFFFFFF -s 16 -d 10 -v
+convert -n FFFFFFFFFF -s 16 -d 10 -v
 pause
+cls
 
-converter -n 0,FFFFF -s 16 -d 10 -v
+convert -n 0,FFFFF -s 16 -d 10 -v
 pause
+cls
+
+convert -n 000000000F,0000F -s 16 -d 2 -v
+pause
+cls
+
+convert -n F000000000,F -s 16 -d 10 -v
+pause
+cls
+
+convert -n F000000000,F -s 16 -d 2 -v
+pause
+cls
